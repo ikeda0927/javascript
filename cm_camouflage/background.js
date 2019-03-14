b1489c5c3b93276a585f54f1f4a1a8495f5e5e39
unknown <ikeda.kohei4477@gmail.com>
2019-03-14 16:09:47 +0900

    Change redirect url

--- cm_camouflage/background.js
+++ cm_camouflage/background.js
@@ -4,14 +4,15 @@ function resetJudge(){
 }
 chrome.webRequest.onBeforeRequest.addListener(
   function (details){
-	console.log(details.url);
+    var today = new Date();
+	console.log(today.getHours() + "h" + today.getMinutes() + "m" + today.getSeconds() + "s,"+details.url);
     if(judge){
       var win = window.open("https://www.ikegon.tk/","safe_area");
       win.close();
       window.open("https://www.ikegon.tk/","safe_area");
       judge = false;
       chrome.alarms.create("alarm1",{delayInMinutes: 5});
-      console.log("ALARM SET.");
+      console.log(today.getHours() + "h" + today.getMinutes() + "m" + today.getSeconds() + "s,"+"ALARM SET.");
     }
   },
   {urls: ["*://trk.ad.abema.io/*"]},
@@ -20,16 +21,27 @@ chrome.webRequest.onBeforeRequest.addListener(
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details){
-	console.log(details.url);
+    var today = new Date();
+	console.log(today.getHours() + "h" + today.getMinutes() + "m" + today.getSeconds() + "s,"+details.url);
     resetJudge();
   },
   {urls: ["*://lk.ameba.jp/*"]},
+  []
+);
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details){
+    var today = new Date();
+	console.log(today.getHours() + "h" + today.getMinutes() + "m" + today.getSeconds() + "s,"+details.url);
+    resetJudge();
+  },
+  {urls: ["*://api.bucketeer.jp/*"]},
   []
 );
 chrome.alarms.onAlarm.addListener(alarm => {
   if(alarm.name === "alarm1"){
     resetJudge();
     chrome.alarms.clearAll();
-    console.log("ALARM FIRED AND CLEARED.");
+    var today = new Date();
+    console.log(today.getHours() + "h" + today.getMinutes() + "m" + today.getSeconds() + "s,"+"ALARM FIRED AND CLEARED.");
   }
 });
